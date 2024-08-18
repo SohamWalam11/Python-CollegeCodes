1561160d30c434d22bb743b131715ef4bd28fafa
SohamWalam11 <147302133+SohamWalam11@users.noreply.github.com>
2024-08-18 07:03:37 +0530

    Delete 0_5.py

--- 0_5.py
+++
@@ -1,7 +0,0 @@
-Princi = int(input("Enter principal amount: "))
-ROI = float(input("Enter Rate of interest: "))
-Period = int(input("Enter number of years: "))
-
-SI = Princi*ROI*Period
-
-print("Simple Interest is: "+str(SI/100))
