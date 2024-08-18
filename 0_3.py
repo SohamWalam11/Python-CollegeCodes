592ebee0fdc260311dce27abc3d338f9543ac180
SohamWalam11 <147302133+SohamWalam11@users.noreply.github.com>
2024-08-18 07:03:19 +0530

    Delete 0_3.py

--- 0_3.py
+++
@@ -1,9 +0,0 @@
-a =input("Enter first number: ")
-b =input("Enter second number: ")
-
-if (a>=b):
-    print(a+' is greater than '+b)
-elif(a<=b):
-    print(a+' is lesser than '+b)
-else:
-    print(a+' is equal to '+b)
