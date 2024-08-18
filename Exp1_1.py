8756fcad95ff066f3bcbdea852382fc07ce017d4
SohamWalam11 <147302133+SohamWalam11@users.noreply.github.com>
2024-08-18 07:03:58 +0530

    Delete Exp1_1.py

--- Exp1_1.py
+++
@@ -1,9 +0,0 @@
-# Exp1_1 - WAP to demonstrate the if,elif,else loop
-a = input("Enter first number: ")
-b = input("Enter second number: ")
-if (a>b):
-    print(a+" is greater than "+b)
-elif(a<b):
-    print(b+' is greater than '+a)
-else:
-    print(a+' is equal to '+b)
